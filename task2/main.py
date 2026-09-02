aa7c5c0c390ec049d3357d4f46eb5fc8455a4067
MashaVyaz <m.vyazigina@gmail.com>
2026-09-02 10:38:04 +0700

    задание 2

---
+++ task2/main.py
@@ -0,0 +1,15 @@
+class Cat:
+    def __init__(self, breed, name, age):
+        self.breed = breed
+        self.name = name
+        self.age = age
+    
+    def draw(self):
+        print(f"На экране рисуется кот {self.name}, порода {self.breed}")
+
+cat1 = Cat("Сиамская", "Шотя", 3)
+cat2 = Cat("Ориентальная", "Китнес", 13)
+cat3 = Cat("Сфинкс", "Пряник", 4)
+cat1.draw()
+cat2.draw()
+cat3.draw()
