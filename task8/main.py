f2f9869b38ba123e4c0db2bf0913e3080b764ea4
MashaVyaz <m.vyazigina@gmail.com>
2026-09-02 12:15:21 +0700

    задание 8

---
+++ task8/main.py
@@ -0,0 +1,45 @@
+class Figure:
+    def __init__(self, coords, width, color):
+        self.coords = coords
+        self.width = width
+        self.color = color
+    def draw(self):
+        print("Рисуется фигура")
+
+class Line(Figure):
+    def __init__(self, coords, width, color, length):
+        super().__init__(coords, width, color)
+        self.length = length
+    def draw(self):
+        print(f"Рисуется линия длиной {self.length}")
+
+class Rect(Figure):
+    def __init__(self, coords, width, color, height):
+        super().__init__(coords, width, color)
+        self.height = height
+    def draw(self):
+        print(f"Рисуется прямоугольник высотой {self.height}")
+
+class Ellipse(Figure):
+    def __init__(self, coords, width, color, radius):
+        super().__init__(coords, width, color)
+        self.radius = radius
+    def draw(self):
+        print(f"Рисуется эллипс с радиусом {self.radius}")
+
+class Triangle(Figure):
+    def __init__(self, coords, width, color, base, height):
+        super().__init__(coords, width, color)
+        self.base = base
+        self.height = height
+    def draw(self):
+        print(f"Рисуется треугольник с основанием {self.base} и высотой {self.height}")
+
+line = Line((0, 50), 5, "розовый", 50)
+rect = Rect((30, 60), 10, "синий", 67)
+ellipse = Ellipse((67, 80), 15, "желтый", 40)
+triangle = Triangle((30, 30), 10, "зеленый", 25, 20) 
+shapes = [line, rect, ellipse, triangle] 
+
+for shape in shapes:
+    shape.draw()
