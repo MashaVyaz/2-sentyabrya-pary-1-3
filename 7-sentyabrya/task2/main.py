8e5d69b2cb0ed71936c03818a7b4ffdb48eecbc9
MashaVyaz <m.vyazigina@gmail.com>
2026-09-07 10:05:16 +0700

    7 сентября, задача 2

---
+++ 7-sentyabrya/task2/main.py
@@ -0,0 +1,14 @@
+class Goods:
+    title = "Мороженое"
+    weight = 154
+    tp = "Еда"
+    price = 1024   
+
+Goods.prise = 2048
+Goods.inflation = 100
+
+print(Goods.title)
+print(Goods.weight)
+print(Goods.tp)
+print(Goods.prise)
+print(Goods.inflation)
